98b6ffd3eb09ee1d6c7603c62e30183ff4fc4c7e
Hassan Khalid <hassan@razgriz.io>
2020-11-20 19:29:45 +0000

    O(N) example with single data structure

--- chapter_03/o_of_n.rb
+++ chapter_03/o_of_n.rb
@@ -1,10 +1,23 @@
-# Example of O(N) algorithm
-def o_of_n
-  things = %w[apples baboons cribs dulcimers]
+# O(N) algorithms
 
+# print items from array
+def print_things(things)
   things.each do |thing|
     p "Here's a thing: #{thing}"
   end
 end
 
+print_things(%w[apples baboons cribs dulcimers])
 
+# detect given number is a prime number
+def is_prime?(number)
+  (2...number).each do |item|
+    if number % item == 0
+      return false
+    end
+  end
+  true
+end
+
+puts "Is 5 a prime number: #{is_prime?(5)}"
+puts "Is 8 a prime number: #{is_prime?(8)}"
