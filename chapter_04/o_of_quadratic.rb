7eed6223a95d286602a4d061955b7065d2ef0a65
Hassan Khalid <hassan@razgriz.io>
2020-11-20 20:14:48 +0000

    added step counter to O(N^2) algorithm

--- chapter_04/o_of_quadratic.rb
+++ chapter_04/o_of_quadratic.rb
@@ -1,10 +1,16 @@
 def has_duplicate_value(array)
+  steps = 0 # count of steps
+
   array.each_index do |i|
     array.each_index do |j|
+      steps += 1
       if i != j && array[i] == array[j]
         return true
       end
     end
   end
+  puts "Total steps: #{steps}"
   return false
 end
+
+has_duplicate_value([1, 4, 5, 2, 9])
