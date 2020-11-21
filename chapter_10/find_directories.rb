63031f1c0946dd8ea8d2c2add70ff0b155dcd2b9
Hassan Khalid <hassan@razgriz.io>
2020-11-21 22:19:45 +0000

    find and print directories recursively

--- chapter_10/find_directories.rb
+++ chapter_10/find_directories.rb
@@ -1,22 +1,11 @@
 def find_directories(directory)
-  # inspect each file within the directory. some of these 'files' may
-  # actually be subdirectories
   Dir.foreach(directory) do |filename|
-
-    # if the current file is itself a subdirectory
     if File.directory?("#{directory}/#{filename}") &&
-        filename != "." && filename != ".."
-
-      # print out the full path name
+        filename != '.' && filename != '..'
       puts "#{directory}/#{filename}"
 
-      # loop through second-level subdirectory
-      Dir.foreach("#{directory}/#{filename}") do |inner_filename|
-        if File.directory?("#{directory}/#{filename}/#{inner_filename}") &&
-            filename != "." && filename != ".."
-            puts "#{directory}/#{filename}/#{inner_filename}"
-        end
-      end
+      # recursively call this function on the subdirectory
+      find_directories("#{directory}/#{filename}")
     end
   end
 end
