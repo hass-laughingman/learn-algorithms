05f9603a8ae539f8c4b7d222e34913420a733cc5
Hassan Khalid <hassan@razgriz.io>
2020-11-22 23:43:00 +0000

    read implemented on th LinkedList

--- chapter_14/linked_list.rb
+++ chapter_14/linked_list.rb
@@ -4,4 +4,22 @@ class LunkedList
   def initialize(first_node)
     @first_node
   end
+
+  def read(index)
+    # we begin at the first node of the list
+    current_node = first_node
+    current_index = 0
+
+    while current_index < index do
+      # we keep following the links of each node until we get to the index we're looking for
+      current_node = current_node.next_node
+      current_index += 1
+
+      # if we're past the end of the list, that means the
+      # value cannot be found in the list, so return nil
+      return nil unless current_node
+    end
+
+    current_node.data
+  end
 end
