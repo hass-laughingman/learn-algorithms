ee226bfbf21bf8d8e422bf0c3d7e28272a5e110e
Hassan Khalid <hassan@razgriz.io>
2020-11-23 12:06:24 +0000

    binary_search_tree search

--- chapter_15/binary_search_tree.rb
+++ chapter_15/binary_search_tree.rb
@@ -4,4 +4,23 @@ class TreeNode
     @left_child = left
     @right_child = right
   end
+
+end
+
+def search(value, node)
+  # base case if the node is nonexistent or 
+  # the value has been found
+  if node is nil || node.value == value
+    node 
+
+  # if the value is less than the current node,
+  # perform search on the left child
+  elsif value < node.value
+    search(value, node.left_child)
+
+  # if the value is greater than the current node,
+  # perform search on the right child
+  else
+    search(value, node.right_child)
+  end
 end
