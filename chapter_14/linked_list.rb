4cbd6c71ec932a52af941e1b867f05864cbb94ae
Hassan Khalid <hassan@razgriz.io>
2020-11-23 00:27:45 +0000

    insert method for LinkedList

--- chapter_14/linked_list.rb
+++ chapter_14/linked_list.rb
@@ -1,3 +1,5 @@
+require_relative './node'
+
 class LunkedList
   attr_accessor :first_node
 
@@ -21,5 +23,59 @@ class LunkedList
     end
 
     current_node.data
+  end
+
+  def index_of(value)
+    # we begin at the first node of the list
+    current_node = first_node
+    current_index = 0
+
+    begin
+      # if we find the data we're looking for, we return it
+      if current_node.data == value
+        return current_index
+      end
+
+      # otherwise, we move on the next node
+      current_node = current_node.next_node
+      current_index += 1
+    end while current_node
+
+    # if we get through the entire list without finding the
+    # data, we return nil
+    
+    nil
+  end
+
+  def insert_at_index(index, value)
+    # we create the new node with the provided value
+    new_node = Node.new(value)
+
+    # if we are inserting at the beginning of the list
+    if index.zero?
+      # have our new node link to what was the first node
+      new_node.next_node = first_node
+      # establish that our new node is now the list's first node
+      first_node = new_node
+      return
+    end
+
+    # if we are inserting anywhere other than the beginning
+    current_node = first_node
+    current_index = 0
+
+    # first, we access the node immediately before where the
+    # new node will go
+    while current_index < (index - 1) do
+      current_node = current_node.next_node
+      current_index += 1
+    end
+
+    # have the new node link to the next node
+    new_node.next_node = current_node.next_node
+
+    # modify the link of the previous node to point to
+    # the new node
+    current_node.next_node = new_node
   end
 end
