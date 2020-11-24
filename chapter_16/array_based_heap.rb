bc6aa792136560deabbaaf5ec83a8acc0b831d4c
Hassan Khalid <hassan@razgriz.io>
2020-11-24 00:31:05 +0000

    inset method for the heap

--- chapter_16/array_based_heap.rb
+++ chapter_16/array_based_heap.rb
@@ -10,5 +10,40 @@ class Heap
   def last_node
     @data.last
   end
-end
 
+  def insert(value)
+    # Turn value into last node my inserting it at the end of the array
+    @data << value
+
+    # keep track of the index of the newly inserted node
+    new_node_index = @data.length - 1
+
+    # the following loop executes the 'trickle up' algorithm
+    # if the new node is not in the root position,
+    # and it's greater than its parent node
+    while new_node_index > 0 &&
+        @data[new_node_index] > @data[parent_index(new_node_index)]
+
+      # swap the new node with the parent node
+      @data[parent_index(new_node_index)], @data[new_node_index] =
+        @data[new_node_index], @dat[parent_index(new_node_index)]
+
+      # update the index of the new node
+      new_node_index = parent_index(new_node_index)
+    end
+  end
+
+  private
+
+  def left_child_index(index)
+    index * 2 + 1
+  end
+
+  def right_child_index(index)
+    index * 2 + 2
+  end
+
+  def parent_index(index)
+    (index - 1) / 2
+  end
+end
