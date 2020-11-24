7fef80fb09b9dc178e49ec929484e9f3e9191b70
Hassan Khalid <hassan@razgriz.io>
2020-11-24 00:59:13 +0000

    delete added to heap

--- chapter_16/array_based_heap.rb
+++ chapter_16/array_based_heap.rb
@@ -33,6 +33,30 @@ class Heap
     end
   end
 
+  def delete
+    # we only ever delete the root node from a heap, so we
+    # pop the last node from the array and make it the root node
+    @data[0] = @data.pop
+
+    # track the current index of the 'tricle node'
+    trickle_node_index = 0
+
+    # the following loop executes the 'tricke down' algorithm
+    # we run the loop as long as the trickle node has a child
+    # that is greater than it
+    while has_greater_child(trickle_node_index)
+      # save larger child index in variable
+      larger_child_index = calculate_larger_child_index(trickle_node_index)
+
+      # swap the tricle node with its larger child
+      @data[trickle_node_index], @data[larger_child_index] =
+        @data[larger_child_index], @data[trickle_node_index]
+
+      # update tricle node's new index
+      trickle_node_index = larger_child_index
+    end
+  end
+
   private
 
   def left_child_index(index)
@@ -45,5 +69,34 @@ class Heap
 
   def parent_index(index)
     (index - 1) / 2
+  end
+
+  def has_greater_child(index)
+    # we check whether the node at index has left and right
+    # children and if either of those children are greater
+    # than the node at index
+    (@data[left_child_index(index)] &&
+      @data[left_child_index(index)] > @data[index]) ||
+      (@data[right_child_index(index)] &&
+      @data[right_child_index(index)] > @data[index])
+  end
+
+  def calculate_larger_child_index(index)
+    # if there is no right child
+    if !@data[right_child_index(index)]
+      # Return left child index
+      return left_child_index(index)
+    end
+
+    # if right child value is greater than left child value
+    if @data[right_child_index(index)] > @data[left_child_index(index)]
+      # return right child index
+      right_child_index(index)
+
+    # if the left child value is greater or equal ro right child
+    else
+      # return the left child index
+      left_child_index(index)
+    end
   end
 end
