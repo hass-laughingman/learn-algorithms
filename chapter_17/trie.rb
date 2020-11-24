3b1a1a86548290b28ea897d56b2f0976096e049a
Hassan Khalid <hassan@razgriz.io>
2020-11-24 02:16:47 +0000

    search added to Trie

--- chapter_17/trie.rb
+++ chapter_17/trie.rb
@@ -8,4 +8,23 @@ class Trie
   def initialize
     @root = TrieNode.new
   end
+
+  def search(word)
+    current_node = root
+
+    word.each do |char|
+      # if the current node has child key with current character
+      if current_node.children.key(char)
+
+        # follow the child node
+        current_node = current_node.children[char]
+      else
+        # if the current character isn't found amoung
+        # the current node's children, our search word
+        # must not be in the trie
+        return nil
+      end
+    end
+    current_node
+  end
 end
