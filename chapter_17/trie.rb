ff32d1144eff41a8a30587c4b8502519643ae817
Hassan Khalid <hassan@razgriz.io>
2020-11-24 02:22:39 +0000

    insert added to Trie

--- chapter_17/trie.rb
+++ chapter_17/trie.rb
@@ -27,4 +27,30 @@ class Trie
     end
     current_node
   end
+
+  def insert(word)
+    current_node = root
+
+    word.each do |char|
+      # if the current node has child key with current character
+      if current_node.children.key(char)
+        # follow the child node
+        current_node = current_node.children[char]
+      
+      else
+        # if the current character isn't found amoung
+        # the current node's children, we add
+        # the character as a new child node
+        new_node = TrieNode.new
+        current_node.children[char] = new_node
+
+        # follow this new node
+        current_node = new_node
+      end
+    end
+
+    # after inserting the entire word into the trie,
+    # we add a * key at the end
+    current_node.children['*'] = nil
+  end
 end
