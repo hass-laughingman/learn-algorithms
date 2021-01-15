9fc36c3560b999bf8bb9b6eccfc8a9a063dc94a8
Hassan Khalid <hassan@razgriz.io>
2021-01-15 19:00:54 +0000

    reverse: loop though the word in reverse order and build string

--- module01/reverse.go
+++ module01/reverse.go
@@ -1,5 +1,7 @@
 package module01
 
+import "strings"
+
 // Reverse will return the provided word in reverse
 // order. Eg:
 //
@@ -7,5 +9,11 @@ package module01
 //   Reverse("alphabet") => "tebahpla"
 //
 func Reverse(word string) string {
-	return ""
+	var sb strings.Builder
+
+	for i := len(word) - 1; i >= 0; i-- {
+		sb.WriteByte(word[i])
+	}
+
+	return sb.String()
 }
