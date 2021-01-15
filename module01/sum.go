8e645bed543fa667d04c34eb742346a594702228
Hassan Khalid <hassan@razgriz.io>
2021-01-15 18:45:49 +0000

    sum: created a recursion example

--- module01/sum.go
+++ module01/sum.go
@@ -3,10 +3,22 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
+	// return sumWithRecursion(numbers)
+	return sumWithLoop(numbers)
+}
+
+func sumWithLoop(numbers []int) int {
 	sum := 0
 	for _, val := range numbers {
 		sum += val
 	}
 	return sum
+}
 
+func sumWithRecursion(numbers []int) int {
+	if len(numbers) == 0 {
+		return 0
+	}
+
+	return numbers[0] + sumWithRecursion(numbers[1:])
 }
